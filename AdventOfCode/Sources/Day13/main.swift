d6d42b3e7b47a1d40e500c3acb1fef871e5a30d9
Stephen H. Gerstacker <stephen@gerstacker.us>
2018-12-13 21:18:00 -0500

    Day 13, both parts

--- AdventOfCode/Sources/Day13/main.swift
+++ AdventOfCode/Sources/Day13/main.swift
@@ -75,6 +75,7 @@ enum Direction {
 }
 
 struct Cart {
+    let id: Int
     let x: Int
     let y: Int
     let direction: Direction
@@ -93,6 +94,12 @@ extension Cart: Comparable {
     }
 }
 
+extension Cart: Equatable {
+    static func == (lhs: Cart, rhs: Cart) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
 extension Cart: CustomStringConvertible {
     var description: String {
         switch direction {
@@ -108,27 +115,12 @@ extension Cart: CustomStringConvertible {
     }
 }
 
-func printTrack(_ track: [[Space]], carts: [Cart]) {
-    for (y, row) in track.enumerated() {
-        let line = row.enumerated().map { (arg0) -> String in
-            let (x, value) = arg0
-            
-            if let cart = carts.first(where: { $0.x == x && $0.y == y }) {
-                return cart.description
-            } else {
-                return value.description
-            }
-        }.joined()
-        
-        print(line)
-    }
-}
-
 // Read the input
 let reader = LineReader(handle: FileHandle.standardInput)
 
 var track: [[Space]] = []
 var carts: [Cart] = []
+var nextID = 0
 
 for (y, line) in reader.enumerated() {
     track.append([Space](repeating: .blank, count: line.count))
@@ -147,16 +139,20 @@ for (y, line) in reader.enumerated() {
             track[y][x] = .intersection
         case "^":
             track[y][x] = .straightNS
-            carts.append(Cart(x: x, y: y, direction: .north, turns: 0))
+            carts.append(Cart(id: nextID, x: x, y: y, direction: .north, turns: 0))
+            nextID += 1
         case "v":
             track[y][x] = .straightNS
-            carts.append(Cart(x: x, y: y, direction: .south, turns: 0))
+            carts.append(Cart(id: nextID, x: x, y: y, direction: .south, turns: 0))
+            nextID += 1
         case "<":
             track[y][x] = .straightEW
-            carts.append(Cart(x: x, y: y, direction: .west, turns: 0))
+            carts.append(Cart(id: nextID, x: x, y: y, direction: .west, turns: 0))
+            nextID += 1
         case ">":
             track[y][x] = .straightEW
-            carts.append(Cart(x: x, y: y, direction: .east, turns: 0))
+            carts.append(Cart(id: nextID, x: x, y: y, direction: .east, turns: 0))
+            nextID += 1
         case " ":
             track[y][x] = .blank
         default:
@@ -169,13 +165,13 @@ for (y, line) in reader.enumerated() {
 for (y, row) in track.enumerated() {
     for (x, value) in row.enumerated() {
         if value == .curveLeft { // "\"
-            if x > 0 && track[y][x - 1] == .straightEW {
+            if x > 0 && (track[y][x - 1] == .straightEW || track[y][x - 1] == .intersection) {
                 track[y][x] = .curveSW
             } else {
                 track[y][x] = .curveNE
             }
         } else if value == .curveRight { // "/"
-            if x > 0 && track[y][x - 1] == .straightEW {
+            if x > 0 && (track[y][x - 1] == .straightEW || track[y][x - 1] == .intersection) {
                 track[y][x] = .curveNW
             } else {
                 track[y][x] = .curveSE
@@ -184,103 +180,158 @@ for (y, row) in track.enumerated() {
     }
 }
 
-// Debug
-printTrack(track, carts: carts)
-
-// Run until there's a collision
-var run = 0
-var currentCarts = carts
-
-while true {
-    print("\nRun \(run)\n")
-    printTrack(track, carts: currentCarts)
+class Course {
+    let spaces: [[Space]]
+    let carts: [Cart]
+    var printRuns: Bool
     
-    // Sort the carts and look for a collision
-    currentCarts.sort()
+    init(spaces: [[Space]], carts: [Cart], printRuns: Bool = false) {
+        self.spaces = spaces
+        self.carts = carts
+        self.printRuns = printRuns
+    }
     
-    for idx in 0 ..< (currentCarts.count - 1) {
-        let lhs = currentCarts[idx]
-        let rhs = currentCarts[idx + 1]
+    func firstCollision() -> (x: Int, y: Int) {
+        var run = 0
+        var currentCarts = carts
         
-        if lhs.x == rhs.x && lhs.y == rhs.y {
-            print("Collision at \(lhs.x), \(lhs.y) on run \(run)")
-            break
+        while true {
+            if printRuns {
+                print("Run: \(run)")
+                printTrack(carts: currentCarts)
+                print()
+            }
+            
+            // Sort the carts and move them, looking for the first collision
+            currentCarts.sort()
+            
+            for idx in 0 ..< currentCarts.count {
+                currentCarts[idx] = moveCart(cart: currentCarts[idx])
+                
+                for collisionIdx in 0 ..< currentCarts.count - 1 {
+                    let lhs = currentCarts[collisionIdx]
+                    let rhs = currentCarts[collisionIdx + 1]
+                    
+                    if lhs.x == rhs.x && lhs.y == rhs.y {
+                        return (x: lhs.x, y: lhs.y)
+                    }
+                }
+            }
+            
+            run += 1
         }
     }
     
-    // Move the carts
-    currentCarts = currentCarts.map { (cart) -> Cart in
-        let trackValue = track[cart.y][cart.x]
+    func lastCart() -> (x: Int, y: Int) {
+        var run = 0
+        var currentCarts = carts
         
+        while true {
+            if printRuns {
+                print("Run: \(run)")
+                printTrack(carts: currentCarts)
+                print()
+            }
+            
+            // Sort the carts, move them, and pop off the collisions
+            currentCarts.sort()
+            
+            var removedIDs: Set<Int> = []
+            
+            for idx in 0 ..< currentCarts.count {
+                // Is this cart already removed?
+                if removedIDs.contains(currentCarts[idx].id) {
+                    continue
+                }
+                
+                // Move the cart
+                currentCarts[idx] = moveCart(cart: currentCarts[idx])
+                
+                // Did we have collisions?
+                for collisionCart in currentCarts {
+                    if collisionCart == currentCarts[idx] {
+                        continue
+                    }
+                    
+                    if collisionCart.x == currentCarts[idx].x && collisionCart.y == currentCarts[idx].y {
+                        removedIDs.insert(currentCarts[idx].id)
+                        removedIDs.insert(collisionCart.id)
+                    }
+                }
+            }
+            
+            // Remove the collided carts
+            currentCarts.removeAll(where: { removedIDs.contains($0.id) })
+            
+            // Did we finish?
+            if currentCarts.count == 1 {
+                return (x: currentCarts[0].x, y: currentCarts[0].y)
+            }
+            
+            run += 1
+        }
+    }
+    
+    private func moveCart(cart: Cart) -> Cart {
+        // Store the next position
         var nextX = cart.x
         var nextY = cart.y
         var nextDirection = cart.direction
         var nextTurns = cart.turns
         
-        switch trackValue {
-        case .curveRight, .curveLeft, .blank:
-            fatalError("Unsupported track value")
-        case .straightNS:
+        // Advance the cart
+        switch cart.direction {
+        case .north:
+            nextY -= 1
+        case .south:
+            nextY += 1
+        case .east:
+            nextX += 1
+        case .west:
+            nextX -= 1
+        }
+        
+        // Rotate if needed
+        switch track[nextY][nextX] {
+        case .straightNS, .straightEW:
+        break // No turn on straights
+        case .curveRight, .curveLeft:
+            fatalError("Cannot turn on an undefined curve")
+        case .curveSE: // "/"
             switch cart.direction {
             case .north:
-                nextY -= 1
-            case .south:
-                nextY += 1
-            default:
-                fatalError("Unsupported straight NS direction")
-            }
-        case .straightEW:
-            switch cart.direction {
-            case .east:
-                nextX += 1
-            case .west:
-                nextX -= 1
-            default:
-                fatalError("Unsupported straight EW direction")
-            }
-        case .curveSE:
-            switch cart.direction {
-            case .north:
-                nextX += 1
                 nextDirection = .east
             case .west:
-                nextY += 1
                 nextDirection = .south
             default:
-                fatalError("Unsupported curve SE direction")
+                fatalError("Invalid direction \(cart.direction) on curve SE @ \(nextX), \(nextY)")
             }
-        case .curveSW:
+        case .curveSW: // "\"
             switch cart.direction {
-            case .east:
-                nextY += 1
-                nextDirection = .south
             case .north:
-                nextX -= 1
                 nextDirection = .west
-            default:
-                fatalError("Unsupported curve SW direction")
-            }
-        case .curveNE:
-            switch cart.direction {
-            case .west:
-                nextY -= 1
-                nextDirection = .north
-            case .south:
-                nextX += 1
-                nextDirection = .east
-            default:
-                fatalError("Unsupported curve NE direction")
-            }
-        case .curveNW:
-            switch cart.direction {
             case .east:
-                nextY -= 1
-                nextDirection = .north
-            case .south:
-                nextX -= 1
-                nextDirection = .west
+                nextDirection = .south
             default:
-                fatalError("Unsupported curve NW direction")
+                fatalError("Invalid direction \(cart.direction) on curve SW @ \(nextX), \(nextY)")
+            }
+        case .curveNE: // "\"
+            switch cart.direction {
+            case .south:
+                nextDirection = .east
+            case .west:
+                nextDirection = .north
+            default:
+                fatalError("Invalid direction \(cart.direction) on curve NE @ \(nextX), \(nextY)")
+            }
+        case .curveNW: // "/"
+            switch cart.direction {
+            case .south:
+                nextDirection = .west
+            case .east:
+                nextDirection = .north
+            default:
+                fatalError("Invalid direction \(cart.direction) on curve NW @ \(nextX), \(nextY)")
             }
         case .intersection:
             switch cart.turns % 3 {
@@ -291,25 +342,39 @@ while true {
             case 2:
                 nextDirection = cart.direction.right
             default:
-                fatalError("Unsupported number of turns")
-            }
-            
-            switch nextDirection {
-            case .north:
-                nextY -= 1
-            case .south:
-                nextY += 1
-            case .east:
-                nextX += 1
-            case .west:
-                nextY += 1
+                fatalError("Invalid number of turns: \(cart.turns) -> \(cart.turns % 3)")
             }
             
             nextTurns += 1
+        case .blank:
+            fatalError("Cannot turn on a blank")
         }
         
-        return Cart(x: nextX, y: nextY, direction: nextDirection, turns: nextTurns)
+        return Cart(id: cart.id, x: nextX, y: nextY, direction: nextDirection, turns: nextTurns)
     }
     
-    run += 1
+    private func printTrack(carts: [Cart]) {
+        for (y, row) in spaces.enumerated() {
+            let line = row.enumerated().map { (arg0) -> String in
+                let (x, value) = arg0
+                
+                if let cart = carts.first(where: { $0.x == x && $0.y == y }) {
+                    return cart.description
+                } else {
+                    return value.description
+                }
+                }.joined()
+            
+            print(line)
+        }
+    }
 }
+
+let course = Course(spaces: track, carts: carts)
+let firstCollision = course.firstCollision()
+
+print("First collision: \(firstCollision)")
+
+let lastCart = course.lastCart()
+
+print("Last cart: \(lastCart)")
